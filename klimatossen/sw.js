5ab136317091526d287edd288411e581a3211d41
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-10-25 09:46:57 +0200

    Cache limit

--- klimatossen/sw.js
+++ klimatossen/sw.js
@@ -1,17 +1,25 @@
 const staticCacheName = "site-static-v1.2";
+const dynamicCacheName = "site-dynamic-v1";
 
 const assets = ["/", "/index.html", "src/component/header/header.module.scss"];
 // kan også linke stylesheets^
+
+caches.open("my-cache").then((cache) => {
+  cache.addAll(assets);
+});
 
 // Install Service Worker
 
 // Install event
 self.addEventListener("install", (event) => {
   console.log("Service Worker has been installed");
+
   event.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       console.log("Write asset files to cache");
-      cache.addAll(assets);
+      cache.addAll(assets).catch((error) => {
+        console.log(error, "Der er en fejl");
+      });
     })
   );
 });
@@ -19,23 +27,48 @@ self.addEventListener("install", (event) => {
 // Activate event
 self.addEventListener("activate", (event) => {
   console.log("Service Worker has been activated");
-
   event.waitUntil(
     caches.keys().then((keys) => {
-      const filteredKeys = keys.filter((key) => key !== staticCacheName);
-      filteredKeys.map((key) => {
-        caches.delete(key);
-      });
+      return Promise.all(
+        keys
+
+          .filter((key) => key !== staticCacheName)
+
+          .map((key) => caches.delete(key))
+      );
     })
   );
 });
 
-// Fetch event
+// Fetch even
 self.addEventListener("fetch", (event) => {
-  console.log("Fetch event", event);
- /*  caches.match(event.request).then(cacheRes => {
-    return(
+  if (!(event.request.url.indexOf("http") === 0)) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cacheRes) => {
+      return (
         cacheRes ||
-    )
-  }) */
+        fetch(event.request).then(async (fetchRes) => {
+          return caches.open(dynamicCacheName).then((cache) => {
+            cache.put(event.request.url, fetchRes.clone());
+
+            return fetchRes;
+          });
+        })
+      );
+    })
+  );
+  // Limit
+  const limitCacheTwo = (cacheName, numberOfAllowedFiles) => {
+    caches.open(cacheName).then((cache) => {
+      cache.keys().then((keys) => {
+        if (keys.length > numberOfAllowedFiles) {
+          cache
+            .delete(keys[0])
+            .then(limitCacheTwo(caches, numberOfAllowedFiles));
+        }
+      });
+    });
+  };
+  limitCacheTwo(dynamicCacheName, 2);
 });
